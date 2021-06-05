8b1b7efbffbb7d7ade08521599b6e44c9e459c4c
mhawry <1245767+mhawry@users.noreply.github.com>
2021-06-05 09:07:22 -0400

    Minor refactor

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import requests
 import tqdm
 import json
 from fake_useragent import UserAgent
+from typing import Optional
 
 LIMIT_PER_PAGE = 100  # we can only fetch 100 posts per call with Reddit's API
 
 
-def fetch_posts(username):
+def fetch_posts(username: str) -> Optional[list]:
     """Fetches all the posts from a user's profile"""
     after = ''
     ua = UserAgent()
@@ -18,12 +19,12 @@ def fetch_posts(username):
 
     # this will loop through the posts page by page
     while after is not None:
-        r = requests.get('https://www.reddit.com/user/{}/submitted/.json?limit={}&after={}'.format(username, LIMIT_PER_PAGE, after),
+        r = requests.get(f"https://www.reddit.com/user/{username}/submitted/.json?limit={LIMIT_PER_PAGE}&after={after}",
                          headers={'User-Agent': ua.random}).json()
 
         # in case the username doesn't exist
         if 'error' in r and r['error'] == 404:
-            logging.error('{} is not a valid Reddit username'.format(username))
+            logging.error(f"{username} is not a valid Reddit username")
             return
 
         for child in r['data']['children']:
@@ -34,7 +35,7 @@ def fetch_posts(username):
     return posts_json
 
 
-def download_posts(username, destination, include_metadata):
+def download_posts(username: str, destination: str, include_metadata: bool) -> None:
     """Crawls through a user's posts and downloads them"""
     posts_json = fetch_posts(username)
 
@@ -48,7 +49,7 @@ def download_posts(username, destination, include_metadata):
         os.mkdir(download_dir)
 
     for post_json in tqdm.tqdm(posts_json,
-                               desc='Downloading posts from user {}'.format(username),
+                               desc=f"Downloading posts from user {username}",
                                unit=' posts',
                                ncols=0):
         if 'url' in post_json['data']:
@@ -62,12 +63,12 @@ def download_posts(username, destination, include_metadata):
                     continue
 
                 r = requests.get(url, allow_redirects=True)
-                file = os.path.join(download_dir, '{}'.format(filename))
+                file = os.path.join(download_dir, filename)
                 open(file, 'wb').write(r.content)
 
                 if include_metadata:
-                    filename = '{}.json'.format(post_json['data']['id'])
-                    file = os.path.join(download_dir, '{}'.format(filename))
+                    filename = f"{post_json['data']['id']}.json"
+                    file = os.path.join(download_dir, filename)
 
                     with open(file, 'w') as json_file:
                         json.dump(post_json['data'], json_file)
@@ -84,35 +85,35 @@ def main():
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
 
-        ap = argparse.ArgumentParser(description='reddit-scraper is a command-line application written in Python that scrapes a Reddit user\'s posts and downloads all images.',
+        ap = argparse.ArgumentParser(description="reddit-scraper is a command-line application written in Python that scrapes a Reddit user's posts and downloads all images.",
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
 
-        ap.add_argument('--username', '-u', help='Username of the Reddit user to scrape.')
-        ap.add_argument('--destination', '-d', default=current_dir, help='Specify the download destination. By default, posts will be stored in <current working directory>/<username>.')
-        ap.add_argument('--include-metadata', '-m', dest='include_metadata', default=False, action='store_true', help='Download the metadata. A JSON file will be created for each post in the same directory as the images.')
+        ap.add_argument('--username', '-u', help="Username of the Reddit user to scrape.")
+        ap.add_argument('--destination', '-d', default=current_dir, help="Specify the download destination. By default, posts will be stored in <current working directory>/<username>.")
+        ap.add_argument('--include-metadata', '-m', dest='include_metadata', default=False, action='store_true', help="Download the metadata. A JSON file will be created for each post in the same directory as the images.")
 
         args = ap.parse_args()
 
         if not args.username:
-            logging.error('You must provide a username')  # username is mandatory
+            logging.error("You must provide a username")  # username is mandatory
             return
 
         if not os.path.exists(args.destination):
-            logging.error('The specified destination does not exist')
+            logging.error("The specified destination does not exist")
             return
 
         if not os.path.isdir(args.destination):
-            logging.error('The specified destination is not a valid directory')
+            logging.error("The specified destination is not a valid directory")
             return
 
-        logging.info('Scraping posts from {}'.format(args.username))
+        logging.info(f"Scraping posts from {args.username}")
 
         # if we reach this point we can start downloading the posts
         download_posts(args.username, args.destination, args.include_metadata)
 
-        logging.info('Scraping complete')
+        logging.info("Scraping complete")
     except KeyboardInterrupt:
-        print('Shutdown requested... exiting')
+        print("Shutdown requested... exiting")
 
     sys.exit(0)
 
